21ca96e2c30dba9421758f0af71cd15036191d8e
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-01-26 17:08:48 +0300

    Correct version

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+
 from .models import Comment, Follow, Post, Group, User
 from .forms import CommentForm, PostForm
 
