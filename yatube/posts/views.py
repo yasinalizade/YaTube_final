1b1d1b3449fb3ddd521005f75a13b90a40b0fbe6
Yasin Alizade <94525961+yasinalizade@users.noreply.github.com>
2022-06-01 10:26:02 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -102,6 +102,7 @@ def post_create(request: HttpRequest) -> HttpResponse:
     return render(request, 'posts/create_post.html', context)
 
 
+@login_required
 def post_edit(request: HttpRequest, post_id: int) -> HttpResponse:
     """Edit post."""
     post = get_object_or_404(Post, id=post_id)
