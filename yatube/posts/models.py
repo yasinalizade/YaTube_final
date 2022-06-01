867d4330a9ced9a5ace32df0c2d65497318946a9
Yasin Alizade <94525961+yasinalizade@users.noreply.github.com>
2022-06-01 10:23:57 +0300

    Small remarks

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,35 +8,37 @@ User = get_user_model()
 
 
 class Group(models.Model):
+    """Model - group."""
     title = models.CharField(
-        verbose_name='Группа',
-        help_text='Выберите группу',
+        verbose_name='Group',
+        help_text='Choose group',
         max_length=200
     )
     slug = models.SlugField(allow_unicode=True, unique=True)
     description = models.TextField()
 
     class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
 
     def __str__(self) -> str:
         return self.title
 
     def __repr__(self) -> str:
-        return f"<Group {self.title}>"
+        return f'<Group {self.title}>'
 
 
 class Post(CreatedModel):
+    """Model - post."""
     text = models.TextField(
-        'Текст',
-        help_text='Введите текст поста'
+        'Text',
+        help_text='Write here'
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts',
-        verbose_name='Автор',
+        verbose_name='Author',
     )
     group = models.ForeignKey(
         Group,
@@ -44,56 +46,58 @@ class Post(CreatedModel):
         blank=True,
         null=True,
         related_name='posts',
-        verbose_name='Группа',
-        help_text='Выберите группу',
+        verbose_name='Group',
+        help_text='Choose group',
     )
     image = models.ImageField(
-        'Картинка',
+        'Image',
         upload_to='posts/',
         blank=True,
     )
 
     class Meta:
         ordering = ('-pub_date',)
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
 
     def __str__(self) -> str:
         return self.text[:15]
 
 
 class Comment(models.Model):
+    """Model - comment."""
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name='Комментарий',
+        verbose_name='Comments',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name='Автор комментария',
+        verbose_name='Comment\'s author',
     )
     text = models.TextField(
-        'Текст',
-        help_text='Введите текст комментария'
+        'Text',
+        help_text='Write your comment here'
     )
     created = models.DateTimeField(
-        'Дата комментария',
+        'Date',
         auto_now_add=True
     )
 
     class Meta:
         ordering = ('-created',)
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
 
     def __str__(self) -> str:
-        return self.text
+        return self.text[:30]
 
 
 class Follow(models.Model):
+    """Model - follow."""
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -109,8 +113,8 @@ class Follow(models.Model):
         constraints = [models.UniqueConstraint(
             fields=['author', 'user'], name='unique_follower')
         ]
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
+        verbose_name = 'Follow'
+        verbose_name_plural = 'Follows'
 
     def __str__(self) -> str:
-        return super().__str__()
+        return f'{self.user} follows {self.author}'
